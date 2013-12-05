bbead096463915ef3cfc8ee7822c3876677a20c5
Willi Schinmeyer <willi@schinmeyer.de>
2013-12-05 15:04:06 +0100

    Comments

--- wcc/Regex.hs
+++ wcc/Regex.hs
@@ -20,7 +20,7 @@ data Eq alphabet => Regex alphabet = NullSet        -- matches nothing
 
 instance (Show alphabet, Eq alphabet) => Show (Regex alphabet) where
     show (EmptyWord)        = "()"
-    show (Symbol s)         = show s --escapeRegexString $ show s
+    show (Symbol s)         = show s
     show (AnySymbol)        = "."
     show (Concat l r)       = show l ++ show r
     show (Repetition r)     = "(" ++ show r ++ ")*"
@@ -28,47 +28,46 @@ instance (Show alphabet, Eq alphabet) => Show (Regex alphabet) where
     show (Intersection l r) = "(" ++ show l ++ "&" ++ show r ++ ")"
     show (Difference l r)   = "(" ++ show l ++ "-" ++ show r ++ ")"
 
-{- TODO remove?
-regexSpecialChars :: String
-regexSpecialChars = "\\.[]+-|*?&" -- + and ? are not actually used for display, but might confuse
-
-escapeRegexString :: String -> String
-escapeRegexString [] = []
-escapeRegexString (x:xs)
-    | x `elem` regexSpecialChars = '\\' : x : escapedXs
-    | otherwise = x : escapedXs
-    where escapedXs = escapeRegexString xs
--}
-
 concat :: Eq alphabet => Regex alphabet -> Regex alphabet -> Regex alphabet
+-- Pre-/suffixing the empty word changes nothing 
 concat EmptyWord regex = regex
 concat regex EmptyWord = regex
+-- Concatenation of NullSet yields nothing since there's nothing in NullSet
 concat NullSet _ = NullSet
 concat _ NullSet = NullSet
 concat l r = Concat l r 
 
 repetition :: Eq alphabet => Regex alphabet -> Regex alphabet
+-- Repeating the empty word changes nothing
 repetition EmptyWord = EmptyWord
+-- Repeating the empty set 0 times yields an empty word, anything more nothing.
 repetition NullSet = EmptyWord
 repetition regex = Repetition regex
 
 union :: Eq alphabet => Regex alphabet -> Regex alphabet -> Regex alphabet
+-- empty set is the neutral element of union
 union NullSet regex = regex
 union regex NullSet = regex
+-- union with yourself changes nothing
 union l r
     | l == r = l
     | otherwise = Union l r
 
 intersection :: Eq alphabet => Regex alphabet -> Regex alphabet -> Regex alphabet
+-- Intersection with an empty set yields an empty set
 intersection NullSet _ = NullSet
 intersection _ NullSet = NullSet
+-- Intersection with yourself changes nothing 
 intersection l r
     | l == r    = l
     | otherwise = Intersection l r
 
 difference :: Eq alphabet => Regex alphabet -> Regex alphabet -> Regex alphabet
+-- You can't remove anything from an empty set
 difference NullSet _ = NullSet
+-- Removing nothing from a set changes nothing
 difference regex NullSet = regex
+-- Removing everything leaves nothing
 difference l r
     | l == r    = NullSet
     | otherwise = Difference l r 
